6e59cf087eb36bb5b1b60530d7f58de892eeb629
NicksyRop <nicksonkipkorir25@gmail.com>
2022-02-19 11:51:15 +0300

    styled the top navbar

--- src/topBar/TopBar.js
+++ src/topBar/TopBar.js
@@ -2,5 +2,31 @@ import React from "react";
 import "./topbar.css";
 
 export default function TopBar() {
-  return <div className="top">TopBar</div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook-f"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImage"
+          src="https://images.unsplash.com/photo-1482361046637-0226fdcfa3b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+        <i className="searchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
 }
